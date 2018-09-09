f302dfb9a9da97f14d33450669f8a63f8f2901bb
Mark Karpeles <magicaltux@gmail.com>
2018-09-09 16:36:04 +0900

    updating doc, allowing stftime.New to receive multiple tags

--- strftime.go
+++ strftime.go
@@ -13,7 +13,9 @@ type Formatter struct {
 	l *strftimeLocaleInfo
 }
 
-// Format will format the time t following strftime format specified in f using language l.
+var EnglishFormatter = &Formatter{englishLocale}
+
+// Format is a shortcut to format a date in a given locale easily. Best performance is achieved by using language constants such as language.AmericanEnglish or language.French.
 func Format(l language.Tag, f string, t time.Time) string {
 	locale, ok := strftimeLocaleTable[l]
 	if !ok {
@@ -27,25 +29,35 @@ func Format(l language.Tag, f string, t time.Time) string {
 	return b.String()
 }
 
-// FormatUS formats time t using format f and English locale.
-func FormatEnglish(f string, t time.Time) string {
-	b := &bytes.Buffer{}
-	strftimeInternal(englishLocale, b, f, t)
-
-	return b.String()
+// EnFormat formats time t using format f and English locale.
+func EnFormat(f string, t time.Time) string {
+	return EnglishFormatter.Format(f, t)
 }
 
-func New(l language.Tag) *Formatter {
-	locale, ok := strftimeLocaleTable[l]
-	if !ok {
-		// need to match locale
-		_, i, _ := strftimeLocaleMatcher.Match(l)
-		locale = strftimeLocaleTable[strftimeLocaleTags[i]]
+// EnFormatF formats time t using format f in English locale and outputs it to the provided io.Writer.
+func EnFormatF(o io.Writer, f string, t time.Time) error {
+	return EnglishFormatter.FormatF(o, f, t)
+}
+
+// New creates a new Formatter by matching given language tags against known tags.
+//
+// One sample use is as follows:
+// t, q, err := language.ParseAcceptLanguage(r.Header.Get("Accept-Language"))
+// f := strftime.New(t...)
+func New(l ...language.Tag) *Formatter {
+	if len(l) == 1 {
+		if locale, ok := strftimeLocaleTable[l[0]]; ok {
+			return &Formatter{locale}
+		}
 	}
+	// need to match locale
+	_, i, _ := strftimeLocaleMatcher.Match(l...)
+	locale := strftimeLocaleTable[strftimeLocaleTags[i]]
 
 	return &Formatter{locale}
 }
 
+// Format formats time using provided format, and returns a string.
 func (obj *Formatter) Format(f string, t time.Time) string {
 	b := &bytes.Buffer{}
 	strftimeInternal(obj.l, b, f, t)
@@ -53,6 +65,7 @@ func (obj *Formatter) Format(f string, t time.Time) string {
 	return b.String()
 }
 
+// FormatF formats time using provided format, and outputs it to the provided io.Writer.
 func (obj *Formatter) FormatF(o io.Writer, f string, t time.Time) error {
 	if b, ok := o.(strftimeWriter); ok {
 		// output implements the necessary methods to write runes & strings
